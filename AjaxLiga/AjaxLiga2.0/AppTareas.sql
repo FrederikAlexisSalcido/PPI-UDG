b600b135e4db43bf841ea63fae22e1a23afd4b58
FREDERIK <frederik960922@gmail.com>
2020-05-19 18:07:53 -0500

    documentos

---
+++ AjaxLiga/AjaxLiga2.0/AppTareas.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 27-04-2020 a las 08:35:09
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `AppTareas`
+--
+CREATE DATABASE IF NOT EXISTS `AppTareas` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `AppTareas`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Tareas`
+--
+
+
+--
+-- Volcado de datos para la tabla `Tareas`
+--
+
+INSERT INTO `Tareas` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Escribir una sistesis', '					   Tengo que escribir una sintesis'),
+(2, 'Programa de arreglos', '					   Hacer el ejemplo de arreglos'),
+(3, 'Ver video youtube matrices', '					   Ver el video en yutube sobre multiplicacion de arreglos'),
+(4, 'Responder el examen diagnostico', '		Responder el examen diagnostico publicado en classroom			   '),
+(5, 'Hacer la presentacion powerpint', '					   Hacer la presentacion de powerpoint para la exposion del proximo martes'),
+(6, 'Ir con Maria y Mario a hacer maqueta', '	Realizar maqueta en equipo junto con Maria y Mario				   ');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Tareas`
+--
+ALTER TABLE `Tareas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Tareas`
+--
+ALTER TABLE `Tareas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ AjaxLiga/AjaxLiga4.0/index.php
@@ -0,0 +1,103 @@
+ <?php 
+// Cargamos LIGA3
+require_once 'LIGA3/LIGA.php';
+
+// Personalizo una conexión a la base de datos (servidor, usuario, contraseña, schema)
+BD('localhost', 'root', '', 'AppTareas');
+ 
+// Configuramos la entidad a usar
+$tabla = 'Tareas';
+$liga  = LIGA($tabla,'order by nombre');
+
+//Si es una petición asíncrona sólo muestro la respuesta
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+
+  if (isset($_GET['nombre'])) {
+	  $resp = $liga->insertar($_GET);
+	     if ($resp == 1) {
+	      echo 'Registro agregado exitosamente.';
+	     } else {
+	      echo 'Error al insertar.';
+	     }
+  }
+  
+  if (isset($_POST['cual'])) {
+     $datos = array($_POST['cual']=>$_POST);
+     $resp = $liga->modificar($datos);
+     if ($resp == 1) {
+      echo 'Registro modificado exitosamente.';
+     } else {
+      echo 'La información no se modificó, intente más tarde.';
+     }
+    }
+exit(0);
+}
+	
+// Imprimo las etiquetas HTML iniciales
+// 3.3 AJAX con LIGA.js
+HTML::cabeceras(array(
+  'title'      =>'Ajax y Liga',
+  'description'=>'Página de pruebas para AJAX y LIGA 3',
+  'css'        =>'util/LIGA.css',
+  'style'      =>'label { width:100px; }'
+  ));
+ echo '<p style="text-align:center"><font color="#6600CC">AppTareas</font></p>';
+ echo "<HR>";
+  
+// Guardo el bufer para colocarlo en el layout
+ob_start();
+
+$campos = array('*');
+  
+echo "<br>\n\n";
+// Formulario para crear nuevas instancias
+$props  = array('form'=>'id="miForma"',
+                'input[nombre]'=>array('required'=>'required'),
+                'input[descripcion]'=>array('required'=>'required')
+			   ); // Termina el array
+HTML::forma($liga,'Registro de '.$tabla,$campos,$props,TRUE,$_GET);
+echo "<br>\n\n";
+
+// Formulario para modificar instancias
+$props  = array('form'=>array('method'=>'POST', 'id'=>'formaModificar'), 'prefid'=>'algo',
+  'input[nombre]'=>array('required'=>'required'));
+$cual   = !empty($_POST['cual']) ? $_POST['cual'] : '';
+$select = HTML::selector($liga, 1, array('select'=>array('name'=>'cual', 'id'=>'algocual'),
+					 'option'=>array('value'=>'@[0]'),
+					 "option@si('$cual'=='@[0]')"=>array('selected'=>'selected'))
+	   );
+$campos = array('cual'=>$select, '*', '-fecha');
+HTML::forma($liga, 'Modificar '.$tabla, $campos, $props, true);
+  
+$cont = ob_get_clean();
+
+
+
+  // Tabla con instancias
+  $cols = array('*','-id','acción'=>'<a class="borrar" href="?id=@[id]">Borrar</a>');
+  $join = array('depende'=>$liga);
+  $pie  = '<th colspan="@[numCols]">Total de instancias: <span id="numReg">@[numReg]</span> </th>';
+  echo "\n";
+  echo '<form id="lista-form">';
+  echo "\n";
+  HTML::tabla($liga, 'Instancias de '.$tabla, $cols, true, $join,$pie);
+  echo "</form>\n";
+  echo "<br>\n\n";
+	 
+  $cont = ob_get_clean();
+
+
+
+// Estuctura el cuerpo de la página
+HTML::cuerpo(array('cont'=>$cont));
+?>
+<script
+  src="https://code.jquery.com/jquery-3.5.0.min.js"
+  integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ="
+  crossorigin="anonymous"></script>
+<script src="App.js">
+</script>
+<?php
+// Cierre de etiquetas HTML
+HTML::pie();
+?>
